d3b93194222fb8bc644f8fa873f471053992dc29
p.gsd <pgsd@disroot.org>
2022-10-17 21:31:59 +0200

    actually doing it in python will be less painful. Implemented the former undone notes system

---
+++ nbrep.py
@@ -0,0 +1,37 @@
+from os.path import isfile,join
+from os import listdir,mkdir
+from datetime import date
+from random import randint
+
+sessions_path = "/home/gsd/.sessions/"
+def gen_data():
+    for i in range(20):
+        day = randint(1,31)
+        month = randint(1,12)
+        file_name = ''.join([sessions_path, str(day), '_', str(month)])
+        print(file_name)
+        f = open(file_name, 'w')
+        f.write('TOREMOVE')
+        f.close()        
+today_day = date.today().strftime("%d")
+today_month = date.today().strftime("%m")
+today = today_day + "_" + today_month
+try:
+    listdir(sessions_path)
+except FileNotFoundError:
+    print("[!] directory does not exists on path yet, creating it...")
+    mkdir(sessions_path)
+session_files = [f for f in listdir(sessions_path) if isfile(join(sessions_path, f))]
+for session in session_files:
+    month = session.split('_')[1]
+    day = session.split('_')[0]
+    if int(month) >= int(today_month):
+        if int(day) >= int(today_day):
+            pass
+    else:
+        # TODO Informs user and add one week to system
+        print('old file !')
+# prev_sessions = [f for f in sessions_files ]
+# TODO Informs user of the notes that aren't in the system
+if today not in session_files:
+    print("[+] today's session does not exists yet, creating from new notes...")
--- nbrep.sh
+++ nbrep.sh
@@ -1,1 +1,6 @@
 #!/bin/bash
+SESSION_DIR="$HOME/.sessions/"
+WORKFILE="$SESSION_DIR$(date +"%m_%d")"
+echo "$WORKFILE"
+# Check for previous sessions files
+# If prev file is found, warn user and add 7 days to the notes inside
