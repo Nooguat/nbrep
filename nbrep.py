4f5a82011fc9b5aa8d2b59c4d66ec3a7aedda7d4
p.gsd <pgsd@disroot.org>
2022-10-23 10:56:24 +0200

    Added the main features ; need to implement the 'add note from vault'

--- nbrep.py
+++ nbrep.py
@@ -49,9 +49,8 @@ def get_understanding():
 """
     Return n new notes from the vault
 """
-def add_note():
+def add_note(nbr_notes=5):
     ...
-
 
 """
     Generate a new speed value based on the current speed, comprehension of user and how much 
@@ -63,38 +62,53 @@ def update_speed(cur_speed, nb_rep, compr_ratio):
 
 
 """
-    Returns array of notes that needs to be review on that date or previously
+    Returns array of indexes of the lines that need to be review on that date. 
+    If any notes that needed to be reviewed previously is found, it is updated to match the next 
+    week of the current date
 """
 def get_review_notes(notes, date):
     res = []
+    index = 0
     for note in notes:
         note_date = datetime.strptime(note.split(':')[1], "%d_%m")
-        # TODO Notes before the current time should be splitted along the next way
-        if note_date <= date:
-            res.append(note)
+        # TODO Notes before the current time should be splitted along the next week
+        if note_date == date:
+            res.append(index)
+        elif note_date < date:
+            print("This note should have been reviewed before, adding it along the next week")
+        index += 1
     return res
 
 """
     Controls the daily session. Takes an array of notes in argument and read them one by one
 """    
-def session(notes, session_file):
-    for note in notes:
-        values = note.split(':')
+def session(notes_index, session_file):
+    for index in notes_index:
+        values = session_file[index].split(':')
         # TODO Replace 5 by value in config
-        if values[2] >= 5:
-            print("This note has been reviewed a lot ! Please update knowledge graph when review is finished")
-        speed = values[3]
+        date = values[1]
         file = values[0]
+        nbr_seen = values[2]
+        speed = values[3]
+        if nbr_seen >= 5:
+            print("This note has been reviewed a lot ! Please update knowledge graph when review is finished")
+
         command = "speedread " + file + " -w " + speed
         os.system(command)
-        speed = update_speed(speed, values[2], get_understanding())
+        speed = update_speed(speed, nbr_seen, get_understanding())
         # TODO Create real model
-        date = compute_date(values[1], (values[2]//10)*13)
+        date = compute_date(date, (nbr_seen//10)*13).strftime("%d_%m")
         # TODO Find how the session file is updated with those new values + removing the one before
+        nbr_seen += 1
+        n_review = ':'.join([file, date,nbr_seen, ])
+        session_file[index] = n_review
+    print("Review is done for today !")
+    return session_file
 
 if __name__ == "__main__":
-    # read_cfg
-    with session_file as open(sessions, 'r+'):
-        session(get_review_notes(session_file.read(), date.today(), session_file)
+    # TODO read_cfg
+    with open(sessions, 'r+') as session_file:
+        n_file = session(get_review_notes(session_file.readlines(), date.today()), session_file.readlines())
+        session_file.write('\n'.join(n_file))
 
         
